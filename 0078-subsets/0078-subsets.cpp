04e17653d926c7f6e6bbba30fa2b688bbace58bf
Aakash Kumar <144116390+Aakash7838@users.noreply.github.com>
2026-08-23 10:46:29 +0530

    Time: 0 ms (100%), Space: 9.9 MB (82.53%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,28 +1,19 @@
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        int n     = (int)nums.size();
-        int total = 1 << n;  // 2^n subsets
+        int n = nums.size();
+        vector<vector<int>> ans;
+        int total = 1 << n;
 
-        // result array — total subsets, har subset mein max n elements
-        vector<vector<int>> result;
-
-        for(int mask = 0; mask < total; mask++){
-
-            // current subset banao
-            vector<int> subset;
-
-            for(int i = 0; i < n; i++){
-
-                // i-th bit set hai mask mein?
-                if(mask & (1<<i)){
-                    subset.push_back(nums[i]);
+        for(int i = 0; i<total; i++){
+            vector<int>temp;
+            for(int j = 0; j < n; j++){
+                if(i & (1 << j)){
+                    temp.push_back(nums[j]);
                 }
             }
-
-            result.push_back(subset);
+            ans.push_back(temp);
         }
-
-        return result;
+        return ans;
     }
 };
