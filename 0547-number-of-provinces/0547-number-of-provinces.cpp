d9bc6eda5ab73901c1faecae6ad268fa2f0a93bd
Aakash Kumar <144116390+Aakash7838@users.noreply.github.com>
2026-09-21 22:21:47 +0530

    Time: 0 ms (100%), Space: 19.5 MB (70.44%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int findCircleNum(vector<vector<int>>&
+                      isConnected) {
+        int n = isConnected.size();
+        DSU dsu(n);
+        int components = n;  // initially sab alag
+
+        for(int i = 0; i < n; i++){
+            for(int j = i+1; j < n; j++){
+                if(isConnected[i][j] == 1){
+                    if(dsu.unite(i, j)){
+                        components--;  // merge hua
+                    }
+                }
+            }
+        }
+
+        return components;
+    }
+
+    // DSU inside
+    struct DSU {
+        vector<int> parent, rank;
+
+        DSU(int n): parent(n), rank(n, 0){
+            for(int i=0; i<n; i++) parent[i]=i;
+        }
+
+        int find(int x){
+            if(parent[x]!=x)
+                parent[x]=find(parent[x]);
+            return parent[x];
+        }
+
+        bool unite(int x, int y){
+            int rx=find(x), ry=find(y);
+            if(rx==ry) return false;
+            if(rank[rx]<rank[ry]) parent[rx]=ry;
+            else if(rank[rx]>rank[ry]) parent[ry]=rx;
+            else { parent[ry]=rx; rank[rx]++; }
+            return true;
+        }
+    };
+};
