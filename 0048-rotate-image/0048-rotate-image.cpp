7cc5a0158414d2317d8d86fda4ea14723ee3c3d2
Aakash Kumar <144116390+Aakash7838@users.noreply.github.com>
2026-09-22 20:34:39 +0530

    Time: 0 ms (100%), Space: 10.4 MB (8.67%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -5,13 +5,16 @@ public:
         int m = matrix[0].size();
 
         for(int i = 0; i < n; i++){
-            for(int j = i; j < m; j++){
-                swap(matrix[i][j], matrix[j][i]);
+            for(int j = i; j < m; j++ ){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
             }
         }
 
         for(int i = 0; i < n; i++){
             reverse(matrix[i].begin(), matrix[i].end());
         }
+
     }
 };
